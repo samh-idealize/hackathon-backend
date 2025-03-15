40fd80b80e74f799ec9c30ac6c51a7ee4e9a5b76
samh-idealize <sam@idealize.com.au>
2025-03-15 22:30:54 +1100

    iniital complete

--- supabase/functions/email-classifier/index.ts
+++ supabase/functions/email-classifier/index.ts
@@ -4,8 +4,10 @@
 
 // Setup type definitions for built-in Supabase Runtime APIs
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import OpenAI from "jsr:@openai/openai";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { Database } from "../database.types.ts";
+import { Console } from "node:console";
 
 interface EmailQueue {
   id: number;
@@ -24,16 +26,61 @@ const supabase = createClient<Database>(
   Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
 );
 
+const client = new OpenAI({
+  apiKey:
+    , // This is the default and can be omitted
+});
+
 Deno.serve(async (req) => {
   // Get the email content
   const data: WebhookPayload = await req.json();
 
-  // TODO: Implement Logic
-
   // 1. Get all categories from the database
+  const { data: categories, error } = await supabase.from("categories").select(
+    "*",
+  );
+
+  // console.log(categories)
+
+  /*
+  Classify the email content into one of of the following categories:
+
+  ID: 1, Name: "Tech Support", Description: "Help with technical issues"
+  ID: 2, Name: "Sales", Description: "Help with purchasing products"
+
+  Return the ID of the category that best fits the email content.
+  */
 
   // 2. Create a prompt for the AI to understand the existing categories
+  let content =
+    "Classify the email content into one of of the following categories: ";
 
+  const categoryListString = categories?.map((category) =>
+    `ID: ${category.id}, Name: "${category.name}", Description: "${category.description}"`
+  ).join("\n");
+
+  content += categoryListString;
+  content +=
+    "\n Return the ID of the category that best fits the email content.";
+
+  console.log(content);
+
+  const completion = await client.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "developer",
+        content: content,
+      },
+      {
+        role: "user",
+        content:
+          "Let me start with a question: Are you having trouble generating leads and experiencing low website traffic?",
+      },
+    ],
+  });
+
+  console.log(completion.choices[0].message.content);
   // 3. AI spits out strucutred IDs
 
   // 4. Get AI generate title and description for email
